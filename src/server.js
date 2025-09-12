61ca5a046ea6ec5085644a5b3682ccb82ae4377c
erikdelens-dot <erikdelens@gmail.com>
2025-09-12 16:25:18 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -45,6 +45,20 @@ global.fetch = (url, options = {}) => {
 const app = express();
 app.use(express.json());
 
+// CORS middleware toevoegen
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-internal-key');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // (Optioneel) simpele header protectie voor alle POST routes
 app.use((req, res, next) => {
   if (INTERNAL_KEY && req.method === 'POST') {
